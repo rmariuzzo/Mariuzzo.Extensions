765928dc1ae4f8afdeab57b68bfd2b40e35c493f
Rubens Mariuzzo <rubens@mariuzzo.com>
2013-03-06 18:14:02 -0400

    Initial project

---
+++ Mariuzzo.Extensions/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Mariuzzo.Extensions")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("Mariuzzo.Extensions")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("15533d22-8007-4b7d-9221-a804f29fbd50")]
+[assembly: AssemblyVersion("13.3.06.*")]
+[assembly: AssemblyFileVersion("13.3.06.*")]
